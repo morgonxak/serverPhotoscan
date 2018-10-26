f3ed72f144f753f53c8e55443f5e016a58eefacd
morgon <morgon08@mail.ru>
2018-10-26 09:16:46 +0700

    Добавлена взаимодействия Pfotoscan и Базы

--- run.py
+++ run.py
@@ -13,74 +13,86 @@ class PhotoscanProcessing(Thread):
     def __init__(self, pachDB, settingPC):
         Thread.__init__(self)
         self.db = self.OpenDB(pachDB, settingPC)
-        #[('C:\\Users\\admin\\PycharmProjects\\photoscan\\app\\static\\images', 'C:\\projectTree', 12)]
         self.settings = self.db.getSettings()
 
     def run(self):
         #while True:
             self.listIDProcessing = self.db.getAllIDForProcessing()
             try:
-                UserIDProcessingPhotoscan = list(self.listIDProcessing[0])[0]
+                UserIDProcessingPhotoscan = list(self.listIDProcessing)[0]
                 print("Начало обработки для ID", UserIDProcessingPhotoscan)
-                self.processingPhotoscan(UserIDProcessingPhotoscan)
+                self.startProcessingPhotoscan(UserIDProcessingPhotoscan)
             except IndexError:
                 pass
                 print("Данных для обработки нет")
-            try:
-                UserIDProcessingExport = list(self.listIDProcessing[1])[0]
-                print("Начало экспорта для ID", UserIDProcessingExport)
-                self.processingExport(UserIDProcessingExport)
-            except IndexError:
-                pass
-                print("Данных для экспорта нет")
 
-    def processingPhotoscan(self, UserID):
+
+    def startProcessingPhotoscan(self, UserID):
         '''
         апуск обработки
         :return:
         '''
 
-        print("Начало создания проекта", UserID)
-        self.db.pullData('treatment', [(UserID, 'Photoscan', 'CreatProject', False)])
-        self.chunk, self.doc = comandPhotoscan.creatProject(self.settings[0][1] + r'\ID_' + str(UserID) + r'\'', 'project')
-        self.db.editDataTreatment((UserID, 'Photoscan', 'CreatProject', True))
-        print("Окончание создания проекта")
+        if not self.db.getNeedProcessing(UserID, self.db.dictProcessingPhotoscan['Photoscan'][1]):
+            print("Начало создания проекта", UserID)
+            self.db.pullData('treatment', [(UserID, 'Photoscan', 'CreatProject', False)])
+            self.db.editDataTreatment((UserID, 'Photoscan', 'CreatProject', False))
+            self.chunk, self.doc = comandPhotoscan.creatProject(self.settings[0][1] + r'\ID_' + str(UserID) + r'\'', 'project')
+            self.db.editDataTreatment((UserID, 'Photoscan', 'CreatProject', True))
+            print("Окончание создания проекта")
+        else:
+            print("Проект уже создан: этап открытия проекта")
+            self.chunk, self.doc = comandPhotoscan.creatProject(self.settings[0][1] + r'\ID_' + str(UserID) + r'\'', 'project')
 
-        print("Начало добовления фото")
-        self.db.pullData('treatment', [(UserID, 'Photoscan', 'AddPhoto', False)])
-        comandPhotoscan.AddPhoto(self.chunk, self.settings[0][1] + '\/ID_' + str(UserID) + '\/' + 'photo')
-        self.db.editDataTreatment((UserID, 'Photoscan', 'AddPhoto', True))
-        print("Конец добовления фото")
+        if not self.db.getNeedProcessing(UserID, self.db.dictProcessingPhotoscan['Photoscan'][2]):
+            print("Начало добовления фото")
+            self.db.pullData('treatment', [(UserID, 'Photoscan', 'AddPhoto', False)])
+            self.db.editDataTreatment((UserID, 'Photoscan', 'AddPhoto', False))
+            comandPhotoscan.AddPhoto(self.chunk, self.settings[0][1] + '\/ID_' + str(UserID) + '\/' + 'photo')
+            self.db.editDataTreatment((UserID, 'Photoscan', 'AddPhoto', True))
+            print("Конец добовления фото")
+        else:
+            print("Уже проделан этот шаг: добовления фото")
 
-        print("начало выравнивание фотографий")
-        self.db.pullData('treatment', [(UserID, 'Photoscan', 'alingPhotos', False)])
-        comandPhotoscan.alingPhotos(self.chunk, self.doc)
-        self.db.editDataTreatment((UserID, 'Photoscan', 'alingPhotos', True))
-        print("Конец выравнивание фотографий")
+        if not self.db.getNeedProcessing(UserID, self.db.dictProcessingPhotoscan['Photoscan'][3]):
+            print("начало выравнивание фотографий")
+            self.db.pullData('treatment', [(UserID, 'Photoscan', 'alingPhotos', False)])
+            self.db.editDataTreatment((UserID, 'Photoscan', 'alingPhotos', False))
+            comandPhotoscan.alingPhotos(self.chunk, self.doc)
+            self.db.editDataTreatment((UserID, 'Photoscan', 'alingPhotos', True))
+            print("Конец выравнивание фотографий")
+        else:
+            print("Уже проделан этот шаг: выравнивание фотографий")
 
-        print("начало выставления системы координат")
-        self.db.pullData('treatment', [(UserID, 'Photoscan', 'CoordinateSystem', False)])
-        comandPhotoscan.setCoordinateSystem(self.chunk, self.doc)
-        self.db.editDataTreatment((UserID, 'Photoscan', 'CoordinateSystem', True))
-        print("Конец выставления системы координат")
+        if not self.db.getNeedProcessing(UserID, self.db.dictProcessingPhotoscan['Photoscan'][4]):
+            print("начало выставления системы координат")
+            self.db.pullData('treatment', [(UserID, 'Photoscan', 'CoordinateSystem', False)])
+            self.db.editDataTreatment((UserID, 'Photoscan', 'CoordinateSystem', False))
+            comandPhotoscan.setCoordinateSystem(self.chunk, self.doc)
+            self.db.editDataTreatment((UserID, 'Photoscan', 'CoordinateSystem', True))
+            print("Конец выставления системы координат")
+        else:
+            print("Уже проделан этот шаг: выставления системы координат")
 
+        if not self.db.getNeedProcessing(UserID, self.db.dictProcessingPhotoscan['Photoscan'][5]):
+            print("Этап построения облака точек")
+            try:
+                print("начало построения плотного облака точек")
+                self.db.pullData('treatment', [(UserID, 'Photoscan', 'buildDenseCloud', False)])
+                self.db.editDataTreatment((UserID, 'Photoscan', 'buildDenseCloud', False))
+                comandPhotoscan.buildDenseCloud(self.chunk, self.doc)
+                self.db.editDataTreatment((UserID, 'Photoscan', 'buildDenseCloud', True))
+                print("Конец построения плотного облака точек")
+            except BaseException:
+                print("Слишком мало данных для построения плотного облака")
+        else:
+            print("Уже проделан этот шаг: построения облака точек")
 
-        try:
-            print("начало построения плотного облака точек")
-            self.db.pullData('treatment', [(UserID, 'Photoscan', 'buildDenseCloud', False)])
-            comandPhotoscan.buildDenseCloud(self.chunk, self.doc)
-            self.db.editDataTreatment((UserID, 'Photoscan', 'buildDenseCloud', True))
-            print("Конец построения плотного облака точек")
-        except RuntimeError:
-            print("шибка мало данных для построения плотного облака")
+        if not self.db.getNeedProcessing(UserID, self.db.dictProcessingPhotoscan['Photoscan'][6]):
+            print("Этап Экспорта данных")
 
-
-    def processingExport(self, UserID):
-        '''
-        Экспортирует данные в нужные папки
-        :return:
-        '''
-        pass
+        else:
+            print("Этап экспорта точек не требуется")
 
     def OpenDB(self, pachDB, settingPC):
         return DBManager(pachDB, settingPC)
--- workWithDataBase.py
+++ workWithDataBase.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 dictProcessingPhotoscan = {'Server': {1: 'DowloadPhoto', 2: 'CreatDirProject'},
-                         'Photoscan':{1: 'CreatProject', 2: 'AddPhoto', 3: 'alingPhotos', 4: 'CoordinateSystem', 5:'buildDenseCloud'}}
+                         'Photoscan':{1: 'CreatProject', 2: 'AddPhoto', 3: 'alingPhotos', 4: 'CoordinateSystem', 5: 'buildDenseCloud', 6: 'Manual processing'}}
 
 class DBManager:
     def __init__(self, pachDB, SettingsPC):
@@ -9,6 +9,8 @@ class DBManager:
         self.SettingsPC = SettingsPC
         self.conn = sqlite3.connect(pachDB, check_same_thread=False)  # или :memory: чтобы сохранить в RAM
         self.cursor = self.conn.cursor()
+        self.dictProcessingPhotoscan = dictProcessingPhotoscan
+        self.killed = False
 
     def getSettings(self):
         wallet = self.cursor.execute(
@@ -18,32 +20,26 @@ class DBManager:
     def pullData(self, DBtable, data):
         '''
         Вставка данных в таблицу принимаетт имя таблицы и [('Exodus', 'Andy Hunter')]
+        [(UserID, 'Photoscan', 'CreatProject', False)]
         :param DBtable:
         :param data:
         :return:
         '''
 
-        query = 'INSERT INTO ' + DBtable + ' VALUES (' + '?,'*(len(data[0])-1) + '?)'
+        wallet = self.cursor.execute("SELECT STATE FROM treatment WHERE CATEGORY = 'Photoscan' AND PROCESS = ? AND ID = ?", (data[0][2], data[0][0]))
 
-        self.cursor.executemany(query, data)
-        self.conn.commit()
+        try:
+            if wallet.fetchall()[0][0] == 1:
+                return True
+        except IndexError:
+            query = 'INSERT INTO ' + DBtable + ' VALUES (' + '?,' * (len(data[0]) - 1) + '?)'
+            self.cursor.executemany(query, data)
+            self.conn.commit()
+
 
     def editDataTreatment(self, data):
         wallet = self.cursor.execute("UPDATE treatment SET STATE = ? WHERE ID = ? AND CATEGORY = ? AND PROCESS = ?", (data[3], data[0], data[1], data[2]))
         self.conn.commit()
-        return wallet.fetchall()
-
-    def getInfoProcess(self, UserID):
-
-        wallet = self.cursor.execute("SELECT CATEGORY, PROCESS, STATE FROM treatment WHERE ID = ?", (UserID,))
-        return wallet.fetchall()
-
-    def getAllUserID(self):
-        wallet = self.cursor.execute("SELECT ID FROM treatment ")
-        return wallet.fetchall()
-
-    def getAllUserIDnoProcessingPhotoscan(self):
-        wallet = self.cursor.execute("SELECT ID FROM treatment WHERE CATEGORY = 'Photoscan'")
         return wallet.fetchall()
 
     def getListKey(self, list):
@@ -57,36 +53,57 @@ class DBManager:
             self.listKey.add(i[0])
         return self.listKey
 
-    def getListIDServer(self, process):
-        wallet = self.cursor.execute("SELECT ID FROM treatment WHERE CATEGORY = 'Server' AND PROCESS = ? AND STATE = 1",(process,))
+    def getAllUserID(self):
+        wallet = self.cursor.execute("SELECT ID FROM treatment")
         return wallet.fetchall()
 
+    def getListIDServer(self, process):
+        wallet = self.cursor.execute("SELECT ID FROM treatment WHERE CATEGORY = 'Server' AND PROCESS = ? AND STATE = 1",(process,))
+        return wallet.fetchone()
+
+    def getNeedProcessing(self, UserID, process):
+        '''
+        True - Такой процесс уже быполнялся и он закончился успешно
+        False - Такого процесса небыло либо он выполнелся неудачно
+        :param UserID:
+        :param process:
+        :return:
+        '''
+        wallet = self.cursor.execute("SELECT STATE FROM treatment WHERE CATEGORY = 'Photoscan' AND PROCESS = ? AND  ID = ?",(process, UserID))
+        try:
+            if wallet.fetchall()[0][0] == 1:
+                return True
+            else:
+                return False
+        except IndexError:
+            return 'NoData'
+
     def getAllIDForProcessing(self):
+        itog = self.getListKey(self.getAllUserID())
+        '''
         listAllUsetId = []
         for testList in dictProcessingPhotoscan['Server']:
-            listAllUsetId.append(self.getListKey(self.getListIDServer(dictProcessingPhotoscan['Server'][testList])))
+            #listAllUsetId.append(self.getListKey(self.getListIDServer(dictProcessingPhotoscan['Server'][testList])))
+            listAllUsetId.append(self.getListIDServer(dictProcessingPhotoscan['Server'][testList]))
             #print('ортировка по ',dictProcessingPhotoscan['Server'][testList],listAllUsetId)
 
         itog = set
         for res in listAllUsetId:
             itog = res.intersection(listAllUsetId[0])
-
+        '''
         return itog
+
 
 if __name__ == "__main__":
     test = DBManager(r'C:\projectTree\database.db', 'PC1')
-    #a = test.getAllIDForProcessingPhotoscan()
-    #b = test.getAllIDForProcessingExportPhotoscan()
-    #print('Нужна обработка photoscan', a)
-    #print('Нужен экспорт данных', b)
 
-    #c = test.getAllIDForProcessing()
-    #print('Пользователи для обработки:', c[0])
-    #print('Пользователи для экспорта:', c[1])
+    UserID = 10
+    #print(test.dictProcessingPhotoscan['Photoscan'][5])
+    #b = test.getNeedProcessing(UserID, test.dictProcessingPhotoscan['Photoscan'][5])
+    #print(b)
+    #d = test.getAllIDForProcessing()
+    #print(type(d))
+    #print(d)
 
-    #UserID = 1
-    #test.pullData('treatment', [(UserID, 'Photoscan', 'CreatProject', False)])
-    #test.editDataTreatment((UserID, 'Photoscan', 'CreatProject', False))
-
-    d = test.test()
-    print(d)
+    c = test.pullData('treatment', [(UserID, 'Photoscan', 'CreatProject', False)])
+    print(c)
